0147567a53cc562fd3315cb65b22280e609d46cc
Numbers <45026831>
2021-09-30 00:53:28 +0100

    Adding girl XP dataset, using moment to determine whether game is currently in DST mode at build time.

--- build/prepend-meta.js
+++ build/prepend-meta.js
@@ -1,13 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 const pkgJson = require('../package.json')
+const moment = require('moment-timezone')
 
 const metaTemplate = fs.readFileSync(path.resolve(__dirname, './hh-plus-plus.meta.template.js'))
 const meta = metaTemplate.toString()
     .replace('{{version}}', pkgJson.version)
     .replace('{{description}}', pkgJson.description)
 const dist = fs.readFileSync(path.resolve(__dirname, '../dist/hh-plus-plus.user.js'))
-const outputFull = `${meta}\n${dist}\n`
+const isDST = moment().tz('Europe/Paris').zoneAbbr() === 'CEST'
+const outputFull = `${meta}
+const isDST = ${isDST}
+${dist}
+`
 
 fs.writeFileSync(path.resolve(__dirname, '../dist/hh-plus-plus.meta.js'), meta)
 fs.writeFileSync(path.resolve(__dirname, '../dist/hh-plus-plus.user.js'), outputFull)
